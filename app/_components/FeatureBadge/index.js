cac385909a13e63d60b37f764b331e9ffaeb7ee3
khanaslam439 <khanaslam439@gmail.com>
2023-12-19 01:52:07 +0530

    modified syncing design

--- app/_components/FeatureBadge/index.js
+++ app/_components/FeatureBadge/index.js
@@ -1,11 +1,14 @@
 export default function FeatureBadge({title, data}){
     return (
-        <div className="mb-8">
-            <h4 className='text-medium font-semibold mb-1'>{title}</h4>
+        <div>
+            <h4 className='text-gray-400 mb-2'>{title}</h4>
             <ul className="flex gap-2">
                 {
-                    data.map(button => (
-                        <li className="p-1 capitalize px-4 bg-slate-200 rounded-lg text-sm text-slate-900">{button}</li>
+                    data.map((btn, id) => (
+                        <li key={id} className="group text-xl font-semibold"><a href="#" className="group flex items-center bg-gray-100 dark:bg-slate-900 hover:text-indigo-600  border-2 border-double border-gray-100 hover:border-indigo-600/30 dark:border-gray-800 hover:dark:border-indigo-600/50 py-1 px-4 rounded-xl align-middle duration-500">
+                        {/* <i className="uil uil-airplay me-2 text-[18px]"></i> */}
+                        <span className="text-[14px] font-semibold">{btn}</span>
+                    </a></li>
                     ))
                 }
             </ul>
