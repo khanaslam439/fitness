c856530d686f9e6700715e8b57092c56ec4f1c86
khanaslam439 <khanaslam439@gmail.com>
2024-03-07 08:10:02 +0530

    classes updates

--- app/_components/Accordion/index.jsx
+++ app/_components/Accordion/index.jsx
@@ -5,9 +5,9 @@ const Accordion = ({title, description}) => {
   return (
     <div className='relative shadow dark:shadow-gray-800 mb-4 rounded-md overflow-hidden'>
         <h2 className="text-base font-semibold mb-0">
-            <button type="button" className="flex justify-between items-center p-5 w-full font-medium text-start bg-gray-50 dark:bg-slate-800 text-indigo-600" onClick={e => setActive((prev) => !prev)}>
+            <button type="button" className="flex justify-between items-center p-3 px-4 w-full font-medium text-start bg-gray-50 dark:bg-slate-800 text-indigo-600" onClick={e => setActive((prev) => !prev)}>
                 <span>{title}</span>
-                <i className="uil uil-angle-down text-2xl"></i>
+                <i className={`uil ${isActive ? 'uil-angle-up' : 'uil-angle-down'} text-2xl`}></i>
             </button>
         </h2>
         <div className={`p-5 ${isActive ? '' : 'hidden'}`}>
